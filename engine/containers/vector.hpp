994382876c04073fee7b6b461f006ed3cba8525a
David Dembner <davidtdembner@gmail.com>
2021-09-20 00:35:09 -0400

    vector container can now allocate memory without object instantiation

--- engine/containers/vector.hpp
+++ engine/containers/vector.hpp
@@ -15,12 +15,15 @@ public:
 
 	vector(const u64& size) : mCapacity(size), mSize(size)
 	{
-		mData = new T[size];
+		mData = reinterpret_cast<T*>(std::malloc(sizeof(T) * mCapacity));
+		for (u64 i = 0; i < mSize; i++) {
+			new (mData + i) T();
+		}
 	}
 
 	~vector()
 	{
-		delete[] mData;
+		std::free(mData);
 		mData = nullptr;
 		mCapacity = 0;
 		mSize = 0;
@@ -29,11 +32,45 @@ public:
 	inline T* data() const noexcept { return mData; }
 	inline u64 capacity() const noexcept { return mCapacity; }
 	inline u64 size() const noexcept { return mSize; }
+	inline u64 max_size() const noexcept { return 0xffffffffffffffff / sizeof(T); }
+
+	void reserve(const u64 newCapacity);
+
+	T& vector<T>::operator[](u64 index) noexcept;
+
+private:
+	void reallocate(const u64 newCapacity);
 
 private:
 	T* mData;
 	u64 mCapacity;
 	u64 mSize;
 };
+
+template<class T>
+inline void vector<T>::reallocate(const u64 newCapacity)
+{
+	mData = reinterpret_cast<T*>(std::malloc(sizeof(T) * newCapacity));
+	mCapacity = newCapacity;
+}
+
+template<class T>
+inline void vector<T>::reserve(const u64 newCapacity)
+{
+	if (newCapacity > capacity()) {
+		if (newCapacity > max_size()) {
+			LOGGER_ERROR("Cannot allocate larger than max allocation size");
+			return;
+		}
+	}
+	
+	reallocate(newCapacity);
+}
+
+template<class T>
+inline T& vector<T>::operator[](u64 index) noexcept
+{
+	return mData[index];
+}
 
 } // namespace gf3d
--- tests/src/test_vector.cpp
+++ tests/src/test_vector.cpp
@@ -3,6 +3,19 @@
 #include <containers/vector.hpp>
 #include <vector> //Test against std::vector
 
+struct TestObject
+{
+	u32 x, y, z;
+
+	TestObject() : x(0), y(1), z(2) {
+		// Default
+	}
+
+	~TestObject() {
+		// Destructor
+	}
+};
+
 u8 VectorCreateAndDestroy()
 {
 	std::vector<u32> stdNumbersEmpty;
@@ -35,7 +48,64 @@ u8 VectorCreateAndDestroy()
 	return TEST_PASS;
 }
 
+u8 VectorIndex()
+{
+	std::vector<u32> stdNumbers(3);
+	gf3d::vector<u32> gf3dNumbers(3);
+
+	stdNumbers[0] = 5;
+	gf3dNumbers[0] = 5;
+
+	should_be_equal(5, gf3dNumbers[0]);
+	should_be_equal(stdNumbers[0], gf3dNumbers[0]);
+
+	stdNumbers[1] = 7;
+	gf3dNumbers[1] = 9;
+
+	should_be_equal(9, gf3dNumbers[1]);
+	should_not_be_equal(stdNumbers[1], gf3dNumbers[1]);
+
+	return TEST_PASS;
+}
+
+u8 VectorObjectConstruct()
+{
+	gf3d::vector<TestObject> gf3dNumbers(3);
+
+	should_be_equal(0, gf3dNumbers[0].x);
+	should_be_equal(1, gf3dNumbers[0].y);
+	should_be_equal(2, gf3dNumbers[0].z);
+
+	should_be_equal(0, gf3dNumbers[1].x);
+	should_be_equal(1, gf3dNumbers[1].y);
+	should_be_equal(2, gf3dNumbers[1].z);
+
+	should_be_equal(0, gf3dNumbers[2].x);
+	should_be_equal(1, gf3dNumbers[2].y);
+	should_be_equal(2, gf3dNumbers[2].z);
+
+	return TEST_PASS;
+}
+
+u8 VectorReserve()
+{
+	std::vector<TestObject> stdNumbers;
+	gf3d::vector<TestObject> gf3dNumbers;
+
+	stdNumbers.reserve(5);
+	gf3dNumbers.reserve(5);
+
+	should_be_equal(5, gf3dNumbers.capacity());
+	should_not_be_equal(5, gf3dNumbers.size());
+	should_not_be_equal(nullptr, reinterpret_cast<void*>(gf3dNumbers.data()));
+
+	return TEST_PASS;
+}
+
 void RegisterVectorTests(TestManager& manager)
 {
 	manager.registerTest(VectorCreateAndDestroy, "vector create and destroy");
+	manager.registerTest(VectorIndex, "vector c array like indexing");
+	manager.registerTest(VectorObjectConstruct, "vector using object constructor and destructor");
+	manager.registerTest(VectorReserve, "vector reserve");
 }
